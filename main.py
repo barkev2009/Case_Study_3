076e72c37f0418bcd4ae78e4d9fff630f9da4781
barkev2009 <barkev2009@bk.ru>
2019-11-17 03:54:48 +0700

    Beginning the project

---
+++ main.py
@@ -0,0 +1,14 @@
+years = int(input("Срок размещения капитала (лет):"))
+initial_capital = float(input("Начальный капитал ($):"))
+percent = float(input("Процентная ставка (%/мес.):"))
+investment_infusion = float(input("Инвестиционные вливания ($/мес.):"))
+
+months = range(1, 13)
+
+capital = initial_capital
+for month in months:
+    init_cap = capital
+    percs = capital * percent
+    capital += percs
+    print('{:.2f} {:.2f} {:.2f}'.format(init_cap, percs, capital))
+    capital += investment_infusion
