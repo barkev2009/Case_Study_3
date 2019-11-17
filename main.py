5184fa37375610a542b04f2623bb9fb49c25af0c
barkev2009 <barkev2009@bk.ru>
2019-11-17 12:20:41 +0700

    Editing

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ initial_capital = float(input("Начальный капитал ($):"))
 percent = float(input("Процентная ставка (%/мес.):"))
 investment_infusion = float(input("Инвестиционные вливания ($/мес.):"))
 
-months = range(1, 13)
+months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь',
+          'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
 
 capital = initial_capital
 for year in range(years):
+    print('{} год'.format(year + 1))
     for month in months:
         init_cap = capital
-        percs = capital * percent
+        percs = capital * percent/100
         capital += percs
-        print('{:.2f} {:.2f} {:.2f}'.format(init_cap, percs, capital))
+        print('\t{}   {:,.2f}    {:,.2f}    {:,.2f}'.format(month, init_cap, percs, capital).replace(',', ' '))
         capital += investment_infusion
