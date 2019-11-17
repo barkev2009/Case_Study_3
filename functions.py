c4881783b78f7153a52d72608add7f4c578fb160
barkev2009 <barkev2009@bk.ru>
2019-11-17 14:33:03 +0700

    Final the terminal display (rus only)

--- functions.py
+++ functions.py
@@ -1,9 +1,14 @@
-from numpy import count_nonzero as count_symbols
+from numpy import count_nonzero
+
+
+def count_symbols(obj):
+    answer = count_nonzero(list(obj.replace(' ', '1')))
+    return answer
 
 
 def print_block(obj, spaces, end=''):
     print('| {}'.format(obj), end='')
-    print(' ' * (spaces - count_symbols(list(str(obj).replace(' ', '1')))), end=end)
+    print(' ' * (spaces - count_symbols(obj)), end=end)
 
 
 def get_maxes(years, initial_capital, percent, investment_infusion):
@@ -13,7 +18,7 @@ def get_maxes(years, initial_capital, percent, investment_infusion):
 
     mon_lists = []
     for month in months:
-        mon_lists.append(2 + count_symbols(list(month)))
+        mon_lists.append(2 + count_symbols(month))
 
     capital = initial_capital
     for year in range(years):
@@ -25,9 +30,9 @@ def get_maxes(years, initial_capital, percent, investment_infusion):
     str_init = '{:,.2f} '.format(init_cap).replace(',', ' ')
     str_percs = '{:,.2f} '.format(percs).replace(',', ' ')
     str_cap = '{:,.2f} '.format(capital).replace(',', ' ')
-    init_cap_sym = count_symbols(list(str_init.replace(' ', '1')))
-    percs_sym = count_symbols(list(str_percs.replace(' ', '1')))
-    capit_sym = count_symbols(list(str_cap.replace(' ', '1')))
+    init_cap_sym = count_symbols(str_init)
+    percs_sym = count_symbols(str_percs)
+    capit_sym = count_symbols(str_cap)
     max_month = max(mon_lists)
     maxes = {'init_cap': init_cap_sym, 'percs': percs_sym, 'capital': capit_sym, 'months': max_month}
     return maxes
--- main.py
+++ main.py
@@ -13,15 +13,18 @@ months = ['январь', 'февраль', 'март', 'апрель', 'май'
 capital = initial_capital
 for year in range(years):
     print('\n{} год'.format(year + 1))
-    print('-' * (9 + maxes['months'] + maxes['init_cap'] + maxes['percs'] + maxes['capital']))
-    print_block('       ', maxes['months'])
-    print_block(' основа ', maxes['init_cap'])
-    print_block(' сумма, % ', maxes['percs'])
-    print_block('         ', maxes['capital'], end='|\n')
-    print_block(' месяц ', maxes['months'])
-    print_block(' инвестиций ', maxes['init_cap'])
-    print_block(' за месяц ', maxes['percs'])
-    print_block(' капитал ', maxes['capital'], end='|\n')
+    print('-' * (9 + max(maxes['months'], count_symbols(' месяц '))
+                 + max(maxes['init_cap'], count_symbols(' инвестиций '))
+                 + max(maxes['percs'], count_symbols(' сумма, % '))
+                 + max(maxes['capital'], count_symbols(' капитал '))))
+    print_block(' ', max(maxes['months'], count_symbols(' месяц ')))
+    print_block(' основа ', max(maxes['init_cap'], count_symbols(' инвестиций ')))
+    print_block(' сумма, % ', max(maxes['percs'], count_symbols(' сумма, % ')))
+    print_block(' ', max(maxes['capital'], count_symbols(' капитал ')), end='|\n')
+    print_block(' месяц ', max(maxes['months'], count_symbols(' месяц ')))
+    print_block(' инвестиций ', max(maxes['init_cap'], count_symbols(' инвестиций ')))
+    print_block(' за месяц ', max(maxes['percs'], count_symbols(' сумма, % ')))
+    print_block(' капитал ', max(maxes['capital'], count_symbols(' капитал ')), end='|\n')
     print('-' * (9 + maxes['months'] + maxes['init_cap'] + maxes['percs'] + maxes['capital']))
 
     for month in months:
@@ -29,12 +32,12 @@ for year in range(years):
         percs = capital * percent/100
         capital += percs
 
-        print_block(str(month), maxes['months'])
+        print_block(str(month), max(maxes['months'], count_symbols(' месяц ')))
         str_init = '{:,.2f}'.format(init_cap).replace(',', ' ')
-        print_block(str_init, maxes['init_cap'])
+        print_block(str_init, max(maxes['init_cap'], count_symbols(' инвестиций ')))
         str_percs = '{:,.2f}'.format(percs).replace(',', ' ')
-        print_block(str_percs, maxes['percs'])
+        print_block(str_percs, max(maxes['percs'], count_symbols(' сумма, % ')))
         str_cap = '{:,.2f}'.format(capital).replace(',', ' ')
-        print_block(str_cap, maxes['capital'], end='|\n')
+        print_block(str_cap, max(maxes['capital'], count_symbols(' капитал ')), end='|\n')
         capital += investment_infusion
     print('-' * (9 + maxes['months'] + maxes['init_cap'] + maxes['capital'] + maxes['percs']))
