fa548626ae266ef9b4cc9c4b25016cf346ee0ab8
barkev2009 <barkev2009@bk.ru>
2019-11-30 02:50:01 +0700

    Something

--- test.py
+++
@@ -1,7 +0,0 @@
-lang_choice = input('Какой язык предпочитаете? What language would you choose? ')
-if lang_choice.lower == 'english' or lang_choice.lower == 'английский':
-    from ENG import *
-elif lang_choice.lower() == 'russian' or lang_choice.lower == 'русский':
-    from RUS import *
-
-print(lang_choice.lower())
